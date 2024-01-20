54a3a4c752008c35569c9f916a4d3ca262b18b83
DrSYakovlev <s.o.yakovlev@gmail.com>
2024-01-20 19:58:19 +0000

    Add functionality to extract market data for the requested day and store in csv file

--- run.py
+++ run.py
@@ -1,31 +1,53 @@
-import openpyxl
-from openpyxl import load_workbook
+import yfinance as yf
+import csv
 
-def access_file():
+def access_data():
     """
-    The function requests the user to provide name and path of the Excel spreadsheet file containing up-to-date trading data of the stock.
-    """    
-    print("Enter path to the folder with your data.\nCopy-paste the path from your explorer to avoid typos:")
-    folder_path = input()
-    print("What is the name of the share price/traded volume file?")
-    file_name = input()
-    file_path = folder_path +  "\\" + file_name + ".xlsx"
-    #print(file_path)
-    file = load_workbook(filename=file_path)
-    sheet = file['Sheet1']
-    #print(file.sheetnames)
-    #for row in sheet.iter_rows(values_only=True):
-    #    print(row)
-    #    print(type(row))
-    #print(type(sheet))    
+    This function accesses the current share price of Ilica plc.
+    """
+    
+    ticker = yf.Ticker('IKA.L').info    
+    current_price = ticker['regularMarketPreviousClose']
+    print(f'Current Ilika share price is...{current_price} p')
+    return current_price
+
+def estimate_ROI ():
+    """
+    This function calculates the current return-on-investment (ROI) calculated from the day of the stock acquisition.
+    """
+    print ('Enter the day of stock aquisition.\nThe day must be provided in a format  YYYY-MM-DD (e.g. 2024-01-05)')
+    acquisition_day = input()
+    print('Retrieving current data info...')
+    acuisition_day_price = yf.download("IKA.L", 2010-5-14, acquisition_day)
+    csv_export = acuisition_day_price.tail()
+    print(csv_export)    
+    csv_export.to_csv("ilika.csv")
+    file = open('ilika.csv')
+    with open("ilika.csv", "r", encoding="utf-8", errors="ignore") as scraped:
+        final_line = scraped.readlines()[-1]
+        print(final_line)
+        print(type(final_line))
+        #final_line = final_line.split
+        historicac_share_price = final_line[5]
+        print(historicac_share_price)
+
+        
+
 
 def main():
     """
     Run all program functions
     """
-    access_file()
+    access_data()
+    estimate_ROI()
 
 
-print("Welcome to the 'Equity Stock Pulse Check' project.\nThis little tool will calculate Return on Investment (ROI) for your share stock based on the date of acquisition and current share price.\nIt will also suggest cell/buy strategy deduced from five-day share price and traded volume extrapolation.\nDISCLAIMER: The algorithm does not take into account unpredictable events, such as breaking news or paiment of dividends.\nTHE RESULTS CANNOT BE TREATED AS A LEGAL FINANCIAL ADVICE.\n")
+
+print("Welcome to the 'Equity Stock Pulse Check' project.\nThis little tool will calculate Return on Investment (ROI) for your share stock based on the date of acquisition and current share price.\nIt will also suggest cell/buy strategy deduced from five-day (tail) share price and traded volume extrapolation.\nDISCLAIMER: The algorithm does not take into account unpredictable events, such as breaking news or paiment of dividends.\nTHE RESULTS CANNOT BE TREATED AS A LEGAL FINANCIAL ADVICE.\n")
+
+print('Press Enter to continue...')
+
+input()
 
 main()
+
